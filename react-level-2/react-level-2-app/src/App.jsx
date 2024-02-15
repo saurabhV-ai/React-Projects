2ac22559f856c64d28b1e834b0bab040852c8d34
Saurabh Vibhute <saurabhvibhute.sv@gmail.com>
2024-02-15 21:40:20 +0530

    Conditional rendering

--- react-level-2/react-level-2-app/src/App.jsx
+++ react-level-2/react-level-2-app/src/App.jsx
@@ -3,7 +3,11 @@ import './App.css'
 import React from 'react'; //Imported React to use <React.Fragment>
 
 function App() {
-  let foodItems = ['Dal','Green Vegetable','Roti','Salad','Milk']
+  // let foodItems = ['Dal','Green Vegetable','Roti','Salad','Milk']
+  let foodItems = []
+  // if(foodItems.length === 0) {
+  //   return <h3>I am hungry</h3>
+  // }
 
   return(
 // div creates extra node in DOM
@@ -13,6 +17,8 @@ function App() {
 // Empty tag is mostly used react fragment and It dont need to import React
   <>
     <h1>Healthy Food</h1>
+    {/* {foodItems.length===0 ? <h3>I am hungry</h3> : null} */}
+    {foodItems.length===0 && <h3>I am hungry</h3> }
     <ul className="list-group">
       {foodItems.map(item => (
         <li key={item} className='list-group-item'>{item}</li>
